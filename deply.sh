eb8735bafd1ce19456d6c5546188ba34283895a5
daw2-garcia23 <garciamanjonjoel@fpllefia.com>
2022-12-05 17:34:35 +0100

    dist

---
+++ deply.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# abortar por errores
+set -e
+
+# build
+npm run build
+
+# navegar al directorio de salida de la compilación
+cd dist
+
+# si está implementando en un dominio personalizado
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# si está implementando en https: // <USERNAME> .github.io
+git push -f https://github.com/daw2-garcia23/Tetris-PrimerCommit main
+
+# si está implementando en https: // <USERNAME> .github.io / <REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
+cd -
